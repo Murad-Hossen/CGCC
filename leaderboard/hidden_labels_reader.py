8f3095b6eec404c9e0b02bbdd27119f3c887f6f9
Murad <murad09buet@gmail.com>
2026-02-12 20:13:26 -0600

    fix environment for reading the hidden labels

--- leaderboard/hidden_labels_reader.py
+++ leaderboard/hidden_labels_reader.py
@@ -7,9 +7,13 @@ load_dotenv()
 
 def read_hidden_labels():
     csv_string = os.environ.get("TEST_LABELS_CSV")
-    if csv_string:
-        csv_buffer = io.StringIO(csv_string)
-        df = pd.read_csv(csv_buffer)
-        return df
-    else:
-        print("Error: TEST_LABELS_CSV not found. Have you added it to your .env file or secrets?")
+    
+    if not csv_string:
+         print("Error: TEST_LABELS_CSV not found. Have you added it to your .env file or secrets?")
+         return None
+     
+    csv_string = csv_string.replace(r'\n', '\n')
+    
+    csv_buffer = io.StringIO(csv_string)
+    df = pd.read_csv(csv_buffer)
+    return df
